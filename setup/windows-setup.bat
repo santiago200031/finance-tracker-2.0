f219757dce3bd63760a926e3ace3b564b60e1d13
Santiago Villavicencio <villavicencioandrs@gmail.com>
2024-10-21 21:08:43 +0200

    Script fixed for java

--- setup/windows-setup.bat
+++ setup/windows-setup.bat
@@ -5,22 +5,24 @@ winget source update
 echo Installing Logi Options+...
 winget install Logitech.OptionsPlus --silent || echo Failed to install Logi Options+
 
+:: Setup Java
 echo Installing Java 21...
-winget install Oracle.JavaRuntimeEnvironment --version 21 --silent || echo Failed to install Java 21
+winget install Oracle.JDK.21 --silent || echo Failed to install Java 21
+
+echo Installing Java Runtime Environment...
+winget install Oracle.JavaRuntimeEnvironment --silent || echo Failed to install JRE
 
 echo Setting JAVA_HOME environment variable...
 setx JAVA_HOME "C:\Program Files\Java\jre-21" /M
 set "PATH=%PATH%;%JAVA_HOME%\bin"
 
+:: Setup Maven
 echo Setting MAVEN_HOME environment variable...
 setx MAVEN_HOME "C:\Program Files\Apache\maven" /M
 set "PATH=%PATH%;%MAVEN_HOME%\bin"
 
 echo Installing Ollama...
 winget install Ollama.Ollama --silent || echo Failed to install Ollama
-
-echo Installing Maven...
-winget install Apache.Maven --silent || echo Failed to install Maven
 
 echo Installing IntelliJ IDEA Ultimate...
 winget install JetBrains.IntelliJIDEA.Ultimate --silent || echo Failed to install IntelliJ IDEA Ultimate
@@ -39,6 +41,9 @@ winget install Git.Git --silent || echo Failed to install Git
 
 echo Installing GitHub CLI...
 winget install GitHub.cli --silent || echo Failed to install GitHub CLI
+
+echo installing RARLab.WinRAR
+winget install RARLab.WinRAR --silent || echo Failed to install RARLab.WinRAR
 
 echo Script execution completed.
 pause
